c7642b6d0e7e2b6da085cc67dcce5423adc50359
Dhruv Seth <dhruvseth2006@gmail.com>
2018-04-17 15:26:57 -0700

    New Dataset, and using LabelEncoder()

--- firstMLapp.py
+++ firstMLapp.py
@@ -3,10 +3,12 @@ import scipy
 import numpy
 import matplotlib
 import sklearn
-import pandas
+import pandas as pd
 
+import matplotlib
 from pandas.plotting import scatter_matrix
 
+from sklearn.preprocessing import LabelEncoder
 from sklearn import model_selection
 from sklearn.metrics import classification_report
 from sklearn.metrics import accuracy_score
@@ -17,39 +19,48 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 
-url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
-col_names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
-dataset = pandas.read_csv(url, names=col_names)
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data"
+col_names = ['age', 'workclass', 'fnl-wgt', 'education', 'education-num', 'marital-status', 'occupation', 'relationship',
+             'race','sex', 'capital-gain', 'capital-loss', 'hrs/week', 'native-country', 'avg-income']
 
-#print(dataset.shape)
+dataset = pd.read_csv(url, names=col_names)
 
-#print(dataset.head())
-#print("------------------------------------------------------------------\n")
+
+for column in dataset.columns:
+    if dataset[column].dtype == type(object):
+        le = LabelEncoder()
+        dataset[column] = le.fit_transform(dataset[column])
+'''
+dataset = pd.DataFrame(url, columns=col_names)
+print(dataset.shape)
+print(dataset.head())
+print("------------------------------------------------------------------\n")
 #print(dataset.describe())
-
-#print(dataset.groupby('class').size())
-
 array = dataset.values
+print(dataset.shape, "\n")
+'''
 
-X = array[:,0:4]
-Y = array[:,4]
-
-validation_size = 0.2
+test_size = 0.2
 seed = 7
+array = dataset.values
+X = array[:, 0:15]
+Y = array[:, 14]
 
-X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+print(dataset.groupby('avg-income').size())
+
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=0.2, random_state=seed)
+
 
 models=[]
 
 models.append(('Logistic Regresion', LogisticRegression()))
-models.append(('Linear Discrminant Analysis', LinearDiscriminantAnalysis()))
 models.append(('KNeighbors Classifier', KNeighborsClassifier()))
 models.append(('Decision Tree Classifier', DecisionTreeClassifier()))
 models.append(('GaussianNB', GaussianNB()))
-models.append(('SVM', SVC()))
 
 results=[]
 names=[]
+
 
 for name, model in models:
     kfold = model_selection.KFold(n_splits=10, random_state=seed)
@@ -59,10 +70,10 @@ for name, model in models:
     msg = "%s: %f " %(name,cv_results.mean())
     print(msg)
 
-knaan = KNeighborsClassifier()
+knaan = DecisionTreeClassifier()
 knaan.fit(X_train, Y_train)
 predictions = knaan.predict(X_validation)
 
-print(accuracy_score(Y_validation, predictions))
+print("\n Accuracy : ",accuracy_score(Y_validation, predictions))
 
-print(classification_report(Y_validation, predictions))
+print("\n Report : ",classification_report(Y_validation, predictions))
